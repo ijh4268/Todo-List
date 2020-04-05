33de2d76401905ad368b52dc5ae2c325d6e66872
Isaac Henry <isaachenry2022@u.northwestern.edu>
2020-04-05 17:46:28 -0500

    Implement adding to list funcitonality

--- app.js
+++ app.js
@@ -3,43 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-function getCurrentDay(day) {
-	switch(day){
-		case 0:
-			return "Sunday";
-		case 1:
-			return "Monday";
-		case 2:
-			return "Tuesday";
-		case 3:
-			return "Wednesday";
-		case 4:
-			return "Thursday";
-		case 5:
-			return "Friday";
-		case 6:
-			return "Saturday";
-	}
-}
+var items = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-	var today = new Date();
-	var currentDay = today.getDay();
-	var currentDayText = getCurrentDay(currentDay);
+	const today = new Date();
+	const options = {
+		weekday: 'long',
+		day: 'numeric',
+		month: 'long'
+	};
+	const currentDate = today.toLocaleDateString("en-US", options);
+	
 
-	res.write(`Today is ${currentDayText}!\n`);
-
-	if (currentDay === 6 || currentDay === 0) {
-		res.write("It's a weekend!");
-	}
-	else {
-		res.write("It's a working day :(");
-	}
-
-	res.send();
+	res.render('list', {
+		day: currentDate,
+		items: items
+	});
 });
 
+app.post("/", (req, res) => {
+	item = req.body.newItem;
+	items.push(item);
 
+	res.redirect("/");
+});
 
 app.listen(3000, () => {
 	console.log("Server started on port 3000");
